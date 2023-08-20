e8470a8ec45192295b58001d571a4296876c563a
RahulVishal <rahulvishal237@gmail.com>
2023-08-21 01:50:54 +0530

    ne

--- 10.py
+++ 10.py
@@ -1,27 +1,23 @@
-
 import numpy as np
-import pandas as pd
+import pandas as  pd
 from sklearn.cluster import KMeans
 
-# Step 1: Create the dataset
-data = {
+df={
     'VAR1': [1.713, 0.180, 0.353, 0.940, 1.486, 1.266, 1.540, 0.459, 0.773],
     'VAR2': [1.586, 1.786, 1.240, 1.566, 0.759, 1.106, 0.419, 1.799, 0.186],
     'CLASS': [0, 1, 1, 0, 1, 0, 1, 1, 1]
 }
 
-df = pd.DataFrame(data)
+df=pd.DataFrame(df)
+X=df[['VAR1','VAR2']]
+y=df["CLASS"]
 
-# Step 2: Perform k-means clustering
-k = 3
-X = df[['VAR1', 'VAR2']]
-kmeans = KMeans(n_clusters=k, random_state=42)
-df['Cluster'] = kmeans.fit_predict(X)
+model=KMeans(n_clusters=3)
+df["Cluster"]=model.fit_predict(X)
 
-# Step 3: Predict classification for VAR1=0.906 and VAR2=0.606
-new_point = np.array([[0.906, 0.606]])
-predicted_cluster = kmeans.predict(new_point)
-print("Predicted Cluster:", predicted_cluster[0])
-
-# Step 4: Display the dataset with assigned clusters
 print(df)
+
+new_point=np.array([[0.906,0.606]])
+predicted=model.predict(new_point)
+
+print("Predicted cluster: ",predicted[0])
